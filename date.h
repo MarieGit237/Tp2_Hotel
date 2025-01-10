1da85ce444ee711a5a0bad9c78a18c70cddc8b55
MarieGit237 <mariegracentep@gmail.com>
2025-01-10 11:17:54 +0100

    Update date.h

--- date.h
+++ date.h
@@ -5,21 +5,24 @@
 
 class Date {
 public:
-   Date(int month=1, int day=1);
+   Date(int year=1, int month=1, int day=1);
+   int year() const;
    int month() const;
    int day() const;
+   void updateYear(int year);
    void updateMonth(int month);
    void updateDay(int day);
    void next();
    void back();
 private:
+   int _year;
    int _month;
    int _day;
 
 };
 
-bool isDate(int month, int day);
-int getDaysInMonth(int month);
+bool isDate(int year, int month, int day);
+int getDaysInMonth(int year, int month);
 int dayOfYear(Date d);
 std::string toString(Date d);
 
