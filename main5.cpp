a3ff7ff9b5927dd80fc0ea0650eb15ead72bd3ea
MarieGit237 <mariegracentep@gmail.com>
2025-01-13 15:51:16 +0100

    Update main5.cpp

--- main5.cpp
+++ main5.cpp
@@ -3,13 +3,14 @@
 int main(){
 
 Date date(12,25);
-Reservation reservation1(date, 10, 190, "ocean11", "chambre2112", "dginhac");
+Reservation reservation1(date, 10, 190, "ocean11", "chambre2112", "dginhac",1900);
 int night=reservation1.getNombreNuits();
 int price=reservation1.getPrixNuit();
 int sum=reservation1.getMontantTotal();
 std::cout<<"le nombre de nuits  s'eleve à: "<< night << std::endl;
 std::cout<<"le prix d'une nuit  s'eleve à: "<< price << std::endl;
 std::cout<<"le montant total de la reservation s'eleve à: "<< sum << std::endl;
-
+int new_sum = reservation1.calculMontantReservation(10,180);
+std::cout<<"le montant total de la reservation s'eleve à: "<< new_sum << std::endl; 
 return 0;
 }
